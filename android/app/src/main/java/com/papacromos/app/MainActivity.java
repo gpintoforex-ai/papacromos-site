af9a928e3bf3fa7f75e8658f81513b5ff3a5d358
gpinto <gpinto@pombaldata.com>
2026-05-14 04:18:32 +0100

    site update

--- android/app/src/main/java/com/papacromos/app/MainActivity.java
+++ android/app/src/main/java/com/papacromos/app/MainActivity.java
@@ -1,5 +1,29 @@
 package com.papacromos.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        applyFixedWebViewScale();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        applyFixedWebViewScale();
+    }
+
+    private void applyFixedWebViewScale() {
+        if (getBridge() == null || getBridge().getWebView() == null) {
+            return;
+        }
+
+        getBridge().getWebView().getSettings().setTextZoom(100);
+        getBridge().getWebView().getSettings().setSupportZoom(false);
+        getBridge().getWebView().getSettings().setBuiltInZoomControls(false);
+        getBridge().getWebView().getSettings().setDisplayZoomControls(false);
+    }
+}
