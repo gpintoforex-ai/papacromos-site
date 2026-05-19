d65fc2b5f444ec09c5b59c43c54547bea720312d
gpinto <gpinto@pombaldata.com>
2026-05-19 04:15:34 +0100

    bump version 2026.09

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -185,7 +185,7 @@ export default function Layout({ currentPage, onNavigate, matchCount, pendingTra
                     </div>
                     <div>
                       <dt>vr</dt>
-                      <dd>2026.08</dd>
+                      <dd>2026.09</dd>
                     </div>
                   </dl>
                   {accountError && <p className="profile-error">{accountError}</p>}
